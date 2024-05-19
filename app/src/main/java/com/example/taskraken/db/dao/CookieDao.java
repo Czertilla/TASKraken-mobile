4d1ca64783bf58dc1b18fff5072070111eb1f9fc
Czertilla <blaztermapskorsak@gmail.com>
2024-05-19 23:06:47 +0300

    feature(CookieDao) add delete method

--- app/src/main/java/com/example/taskraken/db/dao/CookieDao.java
+++ app/src/main/java/com/example/taskraken/db/dao/CookieDao.java
@@ -1,6 +1,7 @@
 package com.example.taskraken.db.dao;
 
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -34,4 +35,6 @@ public interface CookieDao {
     @Update
     public void updateCookie(Cookie... cookies);
 
+    @Delete
+    public void delete(Cookie cookie);
 }
