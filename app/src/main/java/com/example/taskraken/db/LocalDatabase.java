143b709071ec87d78a99b6ff4bd01555457794d8
Czertilla <blaztermapskorsak@gmail.com>
2024-05-19 22:48:45 +0300

    feature(LocalDataBase) add CookieDao

--- app/src/main/java/com/example/taskraken/db/LocalDatabase.java
+++ app/src/main/java/com/example/taskraken/db/LocalDatabase.java
@@ -3,10 +3,20 @@ package com.example.taskraken.db;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import com.example.taskraken.db.dao.CookieDao;
+import com.example.taskraken.db.dao.UserDao;
+import com.example.taskraken.db.model.Cookie;
 import com.example.taskraken.db.model.User;
 
 
-@Database(entities = {User.class}, version = 1)
+@Database(
+        entities = {
+                User.class,
+                Cookie.class
+        },
+        version = 1
+)
 public abstract class LocalDatabase extends RoomDatabase {
-    public abstract User userDao();
+    public abstract UserDao userDao();
+    public abstract CookieDao cookieDao();
 }
