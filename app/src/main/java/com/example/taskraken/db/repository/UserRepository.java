f3a0278da4748bb30d38388f0038075562c33d88
Czertilla <blaztermapskorsak@gmail.com>
2024-05-19 22:47:53 +0300

    refactor(UserRepository) non singleton

--- app/src/main/java/com/example/taskraken/db/repository/UserRepository.java
+++ app/src/main/java/com/example/taskraken/db/repository/UserRepository.java
@@ -1,42 +1,85 @@
 package com.example.taskraken.db.repository;
 
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Room;
+
+import com.example.taskraken.db.LocalDatabase;
 import com.example.taskraken.db.dao.UserDao;
 import com.example.taskraken.db.model.User;
+import com.example.taskraken.network.schemas.users.UserRead;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public class UserRepository implements UserDao {
-    @Override
+public class UserRepository {
+
+    private static UserRepository userRepository;
+    LocalDatabase localDatabase;
+
+    public UserRepository(Context context) {
+        super();
+        localDatabase = Room.databaseBuilder(
+                context,
+                LocalDatabase.class,
+                "localDB"
+        ).allowMainThreadQueries().build();
+    }
+
     public void insert(User user) {
-
+        localDatabase.userDao().insertOne(user);
     }
 
-    @Override
+    public void insert(User... users){
+        localDatabase.userDao().insertAll(users);
+    }
+
+    public void insert(UserRead userResponse){
+        User user = new User(userResponse);
+        insert(user);
+        pickUser(user);
+    }
+
     public List<User> getAllUsers() {
-        return null;
+        return localDatabase.userDao().getAllUsers();
     }
 
-    @Override
     public User getById(UUID id) {
-        return null;
+        return localDatabase.userDao().getById(id).get(0);
     }
 
-    @Override
-    public void updateUser(User user) {
-
+    public void update(User user) {
+        localDatabase.userDao().updateUser(user);
     }
 
-    @Override
+    public void update(UserRead userResponse){
+        update(new User(userResponse));
+    }
+
+    public void update(User... users){
+        localDatabase.userDao().updateUsers(users);
+    }
+
     public User getPickedUser() {
-        return null;
+        List<User> user = localDatabase.userDao().getPickedUser();
+        return user.get(0);
     }
+
     public void pickUser(User user){
         for (User row : getAllUsers()){
             row.unpick();
-            updateUser(row);
+            update(row);
         }
         user.pick();
-        updateUser(user);
+        update(user);
+    }
+
+    public void pickUser(UUID id){
+        User user = getById(id);
+        if (user != null){
+            pickUser(user);
+        }
     }
 }
