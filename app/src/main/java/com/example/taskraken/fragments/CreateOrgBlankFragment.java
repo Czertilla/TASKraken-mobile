3571c719e545a89a9d47d2f319eb4b3a24223cfa
Czertilla <blaztermapskorsak@gmail.com>
2024-05-23 00:03:03 +0300

    refactor(layout) rename fragment_create_org_blank file

--- app/src/main/java/com/example/taskraken/fragments/CreateOrgBlankFragment.java
+++ app/src/main/java/com/example/taskraken/fragments/CreateOrgBlankFragment.java
@@ -34,6 +34,6 @@ public class CreateOrgBlankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_org_blank, container, false);
+        return inflater.inflate(R.layout.fragment_regist_org, container, false);
     }
 }
