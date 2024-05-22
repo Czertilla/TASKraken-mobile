7c0f9768acdb0a805ea21b24d7b43bb25aae44a7
Czertilla <blaztermapskorsak@gmail.com>
2024-05-23 00:00:01 +0300

    style(activity_registry) rename id fieldemail_AV

--- app/src/main/java/com/example/taskraken/activities/VerifyActivity.java
+++ app/src/main/java/com/example/taskraken/activities/VerifyActivity.java
@@ -48,7 +48,7 @@ public class VerifyActivity extends Activity {
         sendButton = this.findViewById(R.id.button_request_token);
         verifyButton = this.findViewById(R.id.verifyButton);
         coolDownTimer = this.findViewById(R.id.text_view_cool_down_AV);
-        emailField = this.findViewById(R.id.fieldemail_AV);
+        emailField = this.findViewById(R.id.field_email_AV);
         tokenField = this.findViewById(R.id.verifyTokenField);
         debugText = this.findViewById(R.id.text_view_debug_AF);
 
