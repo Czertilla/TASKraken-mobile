77b334ab8ee4a939d5d832a82690d861df969f3b
Czertilla <blaztermapskorsak@gmail.com>
2024-06-05 19:48:37 +0300

    chore(NetworkService) add ip

--- app/src/main/java/com/example/taskraken/network/services/NetworkService.java
+++ app/src/main/java/com/example/taskraken/network/services/NetworkService.java
@@ -14,8 +14,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NetworkService {
     private static NetworkService mInstance;
-    private static final String BASE_URL = "http://10.0.2.2:8000";
-//    private static final String BASE_URL = "https://taskraken.czertilla.ru";
+//    using this ip for local debug on emulator. Needs launched webapp on localhost:8000
+//    private static final String BASE_URL = "http://10.0.2.2:8000";
+    private static final String BASE_URL = "https://taskraken.czertilla.ru";
     private final Retrofit mRetrofit;
 
     private NetworkService() {
